c06f50e172e730c0228ec2580c10ef0442a65461
Zen-net <allyofjustice1@gmail.com>
2020-05-18 00:57:10 +0700

    Formalize Color in consts

--- src/utils/consts.java
+++ src/utils/consts.java
@@ -1,15 +1,19 @@
 package utils;
 
 import javafx.scene.paint.Color;
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
 
-public class consts {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class consts{
 
     //Thread Priority
     public static final int LOG_PRIORITY = 8;
     public static final int PAINTER_PRIORITY = 6;
-
-
-    public static final int DISTANCE_FROM_RECT_TO_UPPER_BOUND = 5;
 
 
     //Data properties
@@ -22,12 +26,16 @@ public class consts {
             new Color(0.158, 0.89, 0.96, 0.75);
     public static final Color NORMAL_STROKE_COLOR =
             new Color(0.667, 0.96, 0.189, 1);
+
     // size
     public static final double MINIMUM_RECT_HEIGHT = 0.1D;
     public static final double MAXIMUM_RECT_HEIGHT = 0.95D;
     //status
     public static final int NORMAL_RECT = 0;
 
+    //Add more when have more status
+    public static Color[][] COLORS_ARRAY = { {NORMAL_FILL_COLOR, NORMAL_STROKE_COLOR},
+    };
 
     //Log parameters
     public static final int STANDARD_LOG_RATE = 1;
@@ -38,4 +46,25 @@ public class consts {
     public static final float RATE_DECAY_COEFFICIENT = 0.5F;
 
     public static final int SLEEP_TIME_PER_RATE = 10;
+
+
+
+    public static Color getFill(int status){
+        try {
+            return COLORS_ARRAY[0][status];
+        } catch (Exception e){
+            System.out.println("Can't get color");
+        }
+        return new Color(0,0,0,0);
+    }
+
+    public static Color getStroke(int status){
+        try {
+            return COLORS_ARRAY[1][status];
+        } catch (Exception e){
+            System.out.println("Can't get color");
+        }
+        return new Color(0,0,0,0);
+    }
 }
+
